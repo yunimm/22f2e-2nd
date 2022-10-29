a762cfa40a8e746c48488290146971d54870939f
yunimm <yuu9916@gmail.com>
2022-10-29 14:04:30 +0800

    add file list component

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,7 @@ import {
   Bars3Icon,
   MinusIcon,
   PlusIcon,
+  ArrowLeftIcon,
 } from '@heroicons/react/24/outline';
 
 function App() {
@@ -36,6 +37,7 @@ function App() {
   const [step, setStep] = useState('2');
   const [uploadFile, setUploadFile] = useState(null);
   const [mode, setMode] = useState(null);
+  const [showFileList, setFileList] = useState(false);
   // const [prev, setPrev] = useState(0);
   // const [redo, setRedo] = useState(null);
   return (
@@ -223,10 +225,37 @@ function App() {
             )}
             {step > '1' && (
               <>
-                <div className="h-full bg-gray py-2.5 px-5">
-                  <button className="icon-btn w-8">
-                    <Squares2X2Icon className="text-white" />
-                  </button>
+                <div className="relative h-full bg-gray py-2.5 px-5">
+                  {!showFileList && (
+                    <button
+                      onClick={() => setFileList(true)}
+                      className="icon-btn w-8 text-white"
+                    >
+                      <Squares2X2Icon />
+                    </button>
+                  )}
+                  {showFileList && (
+                    <>
+                      <div className="absolute left-0 top-0 h-full w-[184px] border-r border-blue bg-blue bg-opacity-[0.15]">
+                        <ul className="px-5 py-14">
+                          <li className="flex flex-col items-center">
+                            <div className="h-[132.36px] w-[144px] rounded-[10px] border-2 border-blue bg-blue bg-opacity-[0.15]" />
+                            <span>1/2</span>
+                          </li>
+                          <li className="flex flex-col items-center">
+                            <div className="h-[132.36px] w-[144px] rounded-[10px] border-2 border-blue bg-blue bg-opacity-[0.15]" />
+                            <span>2/2</span>
+                          </li>
+                        </ul>
+                      </div>
+                      <button
+                        onClick={() => setFileList(false)}
+                        className="icon-btn absolute w-8 text-white"
+                      >
+                        <ArrowLeftIcon />
+                      </button>
+                    </>
+                  )}
                 </div>
                 <footer className="flex h-[58px] w-full items-center justify-between bg-white py-[9px]">
                   {/* TODO:封裝樣式 */}
